87b8570da748f79beb5fd302cc7e184aff215bf8
Ray Wach <wach.ray+git@gmail.com>
2023-08-17 13:51:19 -0700

    Script to expose containerized execution environment

--- test.sh
+++ test.sh
@@ -1,6 +1,3 @@
 #!/bin/sh -e
 
-docker run -it --rm --name npm-container --network host -u `id -u`:`id -g`  \
-    -v ~/.npmrc:/home/node/.npmrc -v ~/.npm:/home/node/.npm  \
-    -v `pwd`:`pwd` -w `pwd` --env NODE_OPTIONS=--experimental-vm-modules  \
-    node:lts npm run test $*
+./run.sh npm run test $*
